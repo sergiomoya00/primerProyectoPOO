9546fc201a93f4941e2b3f48f8a1b5c5e0c328cf
sergiomoya00 <43157614+sergiomoya00@users.noreply.github.com>
2020-06-16 20:44:05 -0600

    Inicio, interfaz y clases.

---
+++ src/primerproyectoprogramado/PRIMERPROYECTOPROGRAMADO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectoprogramado;
+
+import primerproyectoprogramado.GUI.ChooseRole;
+
+/**
+ *
+ * @author samoy
+ */
+public class PRIMERPROYECTOPROGRAMADO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ChooseRole principal = new ChooseRole();
+        principal.setVisible(true);
+    }
+
+}
